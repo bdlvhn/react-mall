8cdd182402d3bcc551d8ad0b3637a968811ee48b
bdlvhn <bdlvhn@outlook.com>
2022-04-24 17:00:48 +0900

    react mall start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Navbar, Container, Nav, NavDropdown, Carousel } from "react-bootstrap";
+import "./App.css";
+import { useState } from "react";
+import data from "./data.js";
 
 function App() {
+  let [cs, setCs] = useState([data]);
+
+  const Card = (props) => {
+    return (
+      <div className="col-md-4">
+        <img
+          src={`/images/image0${props.i + 1}.jpg`}
+          width="100%"
+          height="200px"
+        />
+        <h4>{props.row.title}</h4>
+        <p>{props.row.content}</p>
+        <p>{props.row.price}</p>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Console Shop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="background">
+        <h1>Seasonal Sale</h1>
+        <p>lorem</p>
+      </div>
+      <div className="container">
+        <div className="row">
+          {cs[0].map((row, i) => (
+            <Card row={row} i={i} key={i} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 0,
+    title: "PS5",
+    content: "sony",
+    price: 700000,
+  },
+
+  {
+    id: 1,
+    title: "XBox Series X",
+    content: "Microsoft",
+    price: 600000,
+  },
+
+  {
+    id: 2,
+    title: "Switch",
+    content: "Nintendo",
+    price: 300000,
+  },
+];
